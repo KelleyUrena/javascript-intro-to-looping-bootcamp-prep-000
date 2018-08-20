e1e2e1905bfa86a4460081e3987d2446647de2c0
kelleynurena@gmail.com Urena <kelleynurena@gmail.com>
2018-08-20 04:23:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,7 @@
 function forLoop(array) {
-  for(var i=0; i<25; i++) {
-   array.push("I am 1 strange loop."); 
-   array.push("")
+  array.push("I am 1 strange loop."); 
+  for(var i=1; i<25; i++) {
+   array.push(`I am ${i} strange loops.`);
   }
+  return array;
 }
